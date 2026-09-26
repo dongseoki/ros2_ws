2910c7870ee5e3cef04d843608bf865023b81ef1
dongseoki <dongseok.lee.log@gmail.com>
2026-09-26 20:11:14 +0900

    move_turtle 선언 만

--- src/turtlesim_catch_them_all_py/turtlesim_catch_them_all_py/turtle_controller.py
+++ src/turtlesim_catch_them_all_py/turtlesim_catch_them_all_py/turtle_controller.py
@@ -8,6 +8,16 @@ class TurtleControllerNode(Node):
         super().__init__("turtle_controller")  
         self.get_logger().info(f"{self.get_name()} begin")
 
+def move_turtle(turtle_name, x_pos, y_pos):
+    # 1 get turtle_name pos by turtle sim topic
+
+    # x,y 좌표까지 움직일수 있도록 아래에 로직을 작성한다.
+    # 아래 명령어 참고. 아래 명령어의 토픽에 메시지 발행하는 방식으로 작업한다.
+    # ros2 topic pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist \
+    # "{linear: {x: 2.0}, angular: {z: 1.0}}"
+
+    # 거북이가 x,y좌표까지 움직이면 함수를 true를 리턴한다. 에러 발생시 false를 리턴한다.
+    pass
 
 def main(args=None):
     rclpy.init(args=args)
